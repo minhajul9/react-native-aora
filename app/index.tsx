175d60abfe23fd841915186735d6feb5b8d5570c
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2024-05-26 23:42:43 +0600

    fonts and files

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-3xl font-pblack">RootLayout</Text>
+    </View>
+  );
+}

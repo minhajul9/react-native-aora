c3395c03f4c4eda86fe13f760945594adb06137e
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2024-05-27 21:43:28 +0600

    files and folder structure and bottom navigation bar created

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 
@@ -5,6 +6,7 @@ export default function App() {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-3xl font-pblack">RootLayout</Text>
+      <Link href="home">Go to profile</Link>
     </View>
   );
 }

1848c6cf26314c7476fe63451c35c99e5c6094f5
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2024-05-28 22:22:27 +0600

    landing page design

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,49 @@
 import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
+
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from '../constants'
 
 export default function App() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-3xl font-pblack">RootLayout</Text>
-      <Link href="home">Go to profile</Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: '100%' }}>
+        <View className="w-full h-full justify-center items-center px-4">
+
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+
+          <View className="mt-5 relative">
+            <Text className="text-3xl text-white text-center font-bold">
+              Discover Endless Possibilities with {" "}
+
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -right-7 -bottom-1"
+              resizeMode="contain"
+            />
+          </View>
+
+          <Text className="text-center text-[13px] text-gray-100 mt-7 font-pregular">
+            Where Creativity Meets Innovation: Embark on a Journey of Limitless Exploration with Aora
+          </Text>
+
+        </View>
+      </ScrollView>
+
+    </SafeAreaView>
   );
 }

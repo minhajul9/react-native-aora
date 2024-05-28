8d2bd978601a13c7abd9bf295af95803a015f83c
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2024-05-28 22:49:59 +0600

    screenshot added

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 This is an [Expo](https://expo.dev) project created with [`create-expo-app`](https://www.npmjs.com/package/create-expo-app).
 
+# Screenshots
+![landing Page](./ss/page1.jpg)
+
 ## Get started
 
 1. Install dependencies
--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,7 +1,7 @@
 import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 
-const CustomButton = ({ title, handlePress, containerStyle, textStyles, isLoading }) => {
+const CustomButton = ({ title , handlePress, containerStyle, textStyles, isLoading }) => {
     return (
         <TouchableOpacity
             className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center ${containerStyle} ${isLoading ? 'opacity-50' : ''}`}
@@ -11,6 +11,7 @@ const CustomButton = ({ title, handlePress, containerStyle, textStyles, isLoadin
                 onPress={handlePress}
                 className={`font-psemibold text-primary text-lg ${textStyles}`}>{title}
             </Text>
+            
         </TouchableOpacity>
     )
 }

e3784904c7f959ef0ae2b79bbe4af5f199040674
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2024-05-30 23:49:04 +0600

    sign in page load

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 
 const CustomButton = ({ title , handlePress, containerStyle, textStyles, isLoading }) => {

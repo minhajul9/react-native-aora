ac24283ea42b6db036f2d3886738122250e5a220
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2024-06-06 23:47:56 +0600

    checking is logged in and getting user data and store it

--- app/index.tsx
+++ app/index.tsx
@@ -5,8 +5,14 @@ import { images } from '../constants'
 import CustomButton from "@/components/CustomButton";
 
 import { Redirect, router } from "expo-router";
+import { useGlobalContext } from "@/context/GlobalProvider";
 
 export default function App() {
+
+  const {isLoading, isLoggedIn} = useGlobalContext();
+
+  if(!isLoading && isLoggedIn) return <Redirect href='/home' />
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: '100%' }}>

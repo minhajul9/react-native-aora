c40c1b990f39dd9ef14f0977c8a916da4a9c1d34
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2024-06-08 20:09:17 +0600

    video thumbnails loaded and shown, videocard component created

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -7,6 +7,7 @@ import Trending from "@/components/Trending";
 import EmptyState from "@/components/EmptyState";
 import { getAllPosts } from "@/lib/appwrite";
 import useAppwrite from '../../lib/useAppwrite'
+import VideoCard from "@/components/VideoCard";
 
 const Home = () => {
 
@@ -31,7 +32,7 @@ const Home = () => {
         // data={[]}
         keyExtractor={(item) => item.$id}
         renderItem={({ item }) => (
-          <Text key={item.id} className="text-3xl text-white">{item.title}</Text>
+          <VideoCard video={item} />
         )}
         ListHeaderComponent={() => (
           <View className="my-6 px-4 space-y-6">
